a89cbecc49c40ac89784eae1c60928389e453809
Shreyas Minocha <shreyasminocha7@gmail.com>
2017-07-06 17:13:00 +0530

    Split source file into multiple files

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,30 @@
 var gulp = require('gulp'),
-    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
     csscomb = require('gulp-csscomb'),
     uglifycss = require('gulp-uglifycss');
 
+var sources = [
+    "src/meta.css",
+    "src/base.css",
+    "src/typography.css",
+    "src/table.css",
+    "src/image.css"
+];
+
 gulp.task('comb', function() {
-    gulp.src('./src/minima.css')
+    gulp.src(sources)
+        .pipe(concat('minima.css'))
         .pipe(csscomb())
         .pipe(gulp.dest('./dist/'))
 });
 
 gulp.task('uglify', function() {
-    gulp.src('./src/minima.css')
+    gulp.src(sources)
+        .pipe(concat('minima.min.css'))
+        .pipe(csscomb())
         .pipe(uglifycss({
             "uglyComments": true
         }))
-        .pipe(rename('minima.min.css'))
         .pipe(gulp.dest('./dist/'))
 });
 
